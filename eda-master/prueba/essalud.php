70f8265a0078645067b4d47eedcee2a36ee3e9de
edcenteno <edgarcentenoa@gmail.com>
2018-10-18 17:24:40 -0500

    act

--- eda-master/prueba/essalud.php
+++ eda-master/prueba/essalud.php
@@ -1,17 +1,22 @@
 <?php
-$dni=$_POST['dni'];
-include "conex.php";
-  require_once("srchector/autoload.php" );
 
-  $essalud = new \EsSalud\EsSalud();
-  $mintra = new \MinTra\mintra();
+    require_once("./srchector/autoload.php");
+
+    $essalud = new \EsSalud\EsSalud();
+    $mintra = new \MinTra\mintra();
+
+    $dni = "44274795";
 
     $search1 = $essalud->search( $dni );
     $search2 = $mintra->search( $dni );
 
+    //var_dump($search1);
+    var_dump($search2);
 
     if( $search1->success == true )
     {
+        //echo "Hola: " . $search1->Nombres;
+/*
         $nombre = $search1->Nombres;
         $apellido = $search1->ApellidoPaterno;
         $apellidom = $search1->ApellidoMaterno;
@@ -20,20 +25,21 @@ include "conex.php";
 
         $nombres=$nombre . ' ' . $apellido. ' '.$apellidom;
 
-        echo $sentencia="UPDATE historial SET
+       echo $sentencia="UPDATE historial SET
                  nombre = '".$nombres."',
                  fecha_nacimiento = '".$fecha_nacimiento."'
-                 WHERE dni='".$dni."' ";
+                 WHERE dni='".$dni."' ";*/
 
-        $mysqli->query($sentencia) or die ("Error al actualizar datos".mysqli_error($mysqli));
+      //  $mysqli->query($sentencia) or die ("Error al actualizar datos".mysqli_error($mysqli));
     }
 
     if( $search2->success == true )
     {
+
         $nombre = $search2->nombre;
         $apellido = $search2->paterno;
         $apellidom = $search2->materno;
-        $fecha_nacimiento = $search2->nacimiento;
+        $fecha_nacimiento = $search1->nacimiento;
 
        $nombres= $nombre . ' ' . $apellido. ' '.$apellidom;
 
@@ -41,14 +47,4 @@ include "conex.php";
                  nombre = '".$nombres."',
                  fecha_nacimiento = '".$fecha_nacimiento."'
                  WHERE dni='".$dni."' ";
-
-        $mysqli->query($sentencia) or die ("Error al actualizar datos".mysqli_error($mysqli));
-    }
-
-    if($search1->success==false && $search2->success==false)
-    {
-        echo "ERROR : " . $search->message;
-    }
-
-
-
+}
